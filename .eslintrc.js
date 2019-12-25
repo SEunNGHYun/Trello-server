129aaba579157dc0657978c78410d73be56cd3aa
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2019-12-25 16:13:51 +0900

    🔧 초기 설정 3 .eslintrc.js 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,12 @@ module.exports = {
         ecmaVersion: 2018
     },
     rules: {
-        semi: true
+        semi: ["error", "always"],
+        "no-trailing-spaces": 0,
+        "keyword-spacing": 0,
+        "no-unused-vars": 1,
+        "no-multiple-empty-lines": 0,
+        "space-before-function-paren": 0,
+        "eol-last": 0
     }
 }
